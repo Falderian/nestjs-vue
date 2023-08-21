71614efe9b4e607cd631b9f873ffd83bb5579f0d
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-08-21 13:51:46 +0300

    ADD: added timestamps

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,10 +1,12 @@
 import { Dashboard } from '../../dashboards/entities/dashboard.entity';
 import {
   Column,
+  CreateDateColumn,
   Entity,
   JoinColumn,
   OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity()
@@ -20,6 +22,19 @@ export class User {
 
   @Column({ type: 'varchar', length: 30, default: 'user' })
   role: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  public created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  public updated_at: Date;
 
   @OneToMany(() => Dashboard, (dashboard) => dashboard.user, {
     cascade: true,
